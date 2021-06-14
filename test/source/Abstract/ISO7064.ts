1c6983130d0f6d0c6088a1ecee377f275b9c7386
Rogier Spieker <rogier@konfirm.eu>
2021-06-14 09:20:41 +0200

    Ported abstract ISO7064 tests

--- test/source/Abstract/ISO7064.ts
+++ test/source/Abstract/ISO7064.ts
@@ -1,52 +1,52 @@
-/* global source, describe, it, each, expect */
+import * as test from 'tape';
+import each from 'template-literal-each';
+import { ISO7064 } from '../../../main';
 
-const { ISO7064 } = require('../../../main.js');
+const instance = new ISO7064();
 
-describe('ISO7064', () => {
+each`
+	property      | value
+	--------------|-------
+	algorithm     | Custom
+	specification | ISO 7064, Custom
+	designation   | ${0}
+	modulus       | ${undefined}
+	radix         | ${undefined}
+	indices       | ${undefined}
+	alphabet      | ${undefined}
+	double        | ${false}
+`((record) => {
+	const { property, value } = record as any;
+	test(`ISO7064 - has property ${property} with value ${value}`, (t) => {
+		t.true(property in instance);
+		t.equal(instance[property as keyof ISO7064], value);
+
+		t.end();
+	});
+});
+
+each`
+	method
+	-------
+	normalize
+	checksum
+	validate
+	generate
+`((record) => {
+	const { method } = record as any;
+	test(`ISO7064 - has method ${method}`, (t) => {
+		t.true(method in instance);
+		t.equal(typeof instance[method as keyof ISO7064], 'function');
+
+		t.end();
+	});
+});
+
+
+test('ISO7064 - throws if the checksum method is not implemented', (t) => {
 	const instance = new ISO7064();
 
-	each`
-		property      | available | value
-		--------------|-----------|-------
-		algorithm     | yes       | Custom
-		specification | yes       | ISO 7064, Custom
-		designation   | yes       | ${0}
-		modulus       | yes       | ${undefined}
-		radix         | yes       | ${undefined}
-		indices       | yes       | ${undefined}
-		alphabet      | yes       | ${undefined}
-		double        | yes       | ${false}
-	`(
-		'has $property available $available with value $value',
-		({ property, available, value }, next) => {
-			expect(property in instance).to.equal(available === 'yes');
-			expect(instance[property]).to.equal(value);
+	t.throws(() => instance.checksum('foo'), 'Checksum method not implemented');
 
-			next();
-		}
-	);
-
-	each`
-		method
-		-------
-		normalize
-		checksum
-		validate
-		generate
-	`('has method $method', ({ method }, next) => {
-		expect(method in instance).to.be.true();
-		expect(instance[method]).to.be.function();
-
-		next();
-	});
-
-	it('throws if the checksum method is not implemented', (next) => {
-		const instance = new ISO7064();
-
-		expect(() => instance.checksum('foo')).to.throw(
-			'Checksum method not implemented'
-		);
-
-		next();
-	});
+	t.end();
 });
