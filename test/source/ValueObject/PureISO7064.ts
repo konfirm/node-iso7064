2f8787763dbc0898ea81f3d2ec2dd28788dbb1c7
Rogier Spieker <rogier@konfirm.eu>
2021-06-14 08:28:13 +0200

    Ported Hybrid/Pure tests

--- test/source/ValueObject/PureISO7064.ts
+++ test/source/ValueObject/PureISO7064.ts
@@ -1,50 +1,49 @@
-/* global source, describe, it, each, expect */
+import * as test from 'tape';
+import each from 'template-literal-each';
+import { ISO7064, PureISO7064 } from '../../../main';
 
-const { ISO7064, PureISO7064 } = require('../../../main.js');
+const instance = new PureISO7064();
 
-describe('ISO7064', () => {
-	describe('Pure', () => {
-		const instance = new PureISO7064();
+test('ISO7064/Pure - is an instance of ISO7064', (t) => {
+	t.true(PureISO7064.prototype instanceof ISO7064);
+	t.true(instance instanceof ISO7064);
 
-		it('is an instance of ISO7064', (next) => {
-			expect(PureISO7064.prototype).to.be.instanceof(ISO7064);
-			expect(instance).to.be.instanceof(ISO7064);
+	t.end();
+});
 
-			next();
-		});
+each`
+	property      | available | value
+	--------------|-----------|-------
+	algorithm     | yes       | Custom
+	specification | yes       | ISO 7064, Custom
+	designation   | yes       | ${0}
+	modulus       | yes       | ${undefined}
+	radix         | yes       | ${2}
+	indices       | yes       | ${undefined}
+	alphabet      | yes       | ${undefined}
+	double        | yes       | ${false}
+`((record) => {
+	const { property, available, value } = record as any;
+	test(`has ${property} available ${available} with value ${value}`, (t) => {
+		t.equal(property in instance, available === 'yes');
+		t.equal(instance[property as keyof PureISO7064], value);
 
-		each`
-			property      | available | value
-			--------------|-----------|-------
-			algorithm     | yes       | Custom
-			specification | yes       | ISO 7064, Custom
-			designation   | yes       | ${0}
-			modulus       | yes       | ${undefined}
-			radix         | yes       | ${2}
-			indices       | yes       | ${undefined}
-			alphabet      | yes       | ${undefined}
-			double        | yes       | ${false}
-		`(
-			'has $property available $available with value $value',
-			({ property, available, value }, next) => {
-				expect(property in instance).to.equal(available === 'yes');
-				expect(instance[property]).to.equal(value);
-
-				next();
-			}
-		);
-
-		each`
-			method
-			-------
-			normalize
-			checksum
-			validate
-			generate
-		`('has method $method', ({ method }, next) => {
-			expect(instance[method]).to.be.function();
-
-			next();
-		});
+		t.end();
 	});
 });
+
+each`
+	method
+	-------
+	normalize
+	checksum
+	validate
+	generate
+`((record) => {
+	const { method } = record as any;
+	test(`has method ${method}`, (t) => {
+		t.equal(typeof instance[method as keyof PureISO7064], 'function');
+
+		t.end();
+	});
+})
